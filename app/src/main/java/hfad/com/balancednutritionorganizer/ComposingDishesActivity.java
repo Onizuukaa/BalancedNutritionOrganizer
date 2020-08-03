91129883ba612841ed7fe253d87cd4417536f97b
Jarek Kowalski <jarekkowalskie@gmail.com>
2020-08-03 22:29:07 +0200

    Commit dla rukiaaa

--- app/src/main/java/hfad/com/balancednutritionorganizer/ComposingDishesActivity.java
+++ app/src/main/java/hfad/com/balancednutritionorganizer/ComposingDishesActivity.java
@@ -21,6 +21,7 @@ public class ComposingDishesActivity extends AppCompatActivity {
     double caloriesSum, carbohydratesSum, gramSum, sugarSum, fatsSum, saturatedFatsSum, proteinSum;
     DecimalFormat format;
     String zmiennaTestowaDlaGitHuba;
+    String zmiennaTestowaDlaGitHub2;
 
     private ArrayList<String> productName = new ArrayList<>();
     private ArrayList<String> productCaloriesArrayList = new ArrayList<>();
