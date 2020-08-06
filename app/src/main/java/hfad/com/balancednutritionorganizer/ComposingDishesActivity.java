fa430eb926bfb03869417c7fb497144236aa4781
Dawid Hajder <dawidhajder@gmail.com>
2020-08-06 10:16:25 +0200

    Dodane usuwanie wierszy w ComposhingDishesActivity i odświeżanie layoutu.

--- app/src/main/java/hfad/com/balancednutritionorganizer/ComposingDishesActivity.java
+++ app/src/main/java/hfad/com/balancednutritionorganizer/ComposingDishesActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,6 +24,9 @@ public class ComposingDishesActivity extends AppCompatActivity {
             textViewComposhingDishesProtein, textViewNoData;
     double caloriesSum, carbohydratesSum, gramSum, sugarSum, fatsSum, saturatedFatsSum, proteinSum;
     DecimalFormat format;
+    EditText editText_removeItem;
+    Button button_removeItem;
+    RecyclerViewAdapter2 adapter;
 
     private ArrayList<String> productName = new ArrayList<>();
     private ArrayList<String> productCaloriesArrayList = new ArrayList<>();
@@ -51,9 +56,66 @@ public class ComposingDishesActivity extends AppCompatActivity {
         textViewComposhingDishesProtein = (TextView) findViewById(R.id.textViewComposhingDishesProtein);
         textViewNoData = (TextView) findViewById(R.id.textViewNoData);
         textViewNoData.setVisibility(View.INVISIBLE);
+        editText_removeItem = (EditText) findViewById(R.id.editText_removeItem);
+        button_removeItem = (Button) findViewById(R.id.button_removeItem);
+
+        button_removeItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = Integer.parseInt(editText_removeItem.getText().toString());
+                buttonRemoveItem(position);
+            }
+        });
 
         initRecyclerView();
+        sumAndViewMacros();
+    }
 
+    private void buttonRemoveItem(int position) {
+        productName.remove(position);
+        productCaloriesArrayList.remove(position);
+        productCarbohydratesArrayList.remove(position);
+        productSugarArrayList.remove(position);
+        productFatsArrayList.remove(position);
+        productSaturatedFatsArrayList.remove(position);
+        productProteinArrayList.remove(position);
+        adapter.notifyItemRemoved(position);
+
+        caloriesSum = 0.0;
+        carbohydratesSum = 0.0;
+        gramSum = 0.0;
+        sugarSum = 0.0;
+        fatsSum = 0.0;
+        saturatedFatsSum = 0.0;
+        proteinSum = 0.0;
+
+        sumAndViewMacros();
+    }
+
+    private void initRecyclerView() {
+        RecyclerView recyclerView = findViewById(R.id.composingDishesRecyclerView);
+        adapter = new RecyclerViewAdapter2(this, mProductImage, productName, productCaloriesArrayList, productCarbohydratesArrayList,
+                productSugarArrayList, productFatsArrayList, productSaturatedFatsArrayList, productProteinArrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        if (productName.isEmpty()) {
+            textViewNoData.setVisibility(View.VISIBLE);
+        }
+    }
+
+    private void getIncomingIntent() {
+        productName = getIntent().getStringArrayListExtra("product_name");
+        imageUrl = getIntent().getStringExtra("product_image");
+        productCaloriesArrayList = getIntent().getStringArrayListExtra("product_calories");
+        productGramArrayList = getIntent().getStringArrayListExtra("product_gram");
+        productCarbohydratesArrayList = getIntent().getStringArrayListExtra("product_carbohydrates");
+        productSugarArrayList = getIntent().getStringArrayListExtra("product_sugar");
+        productFatsArrayList = getIntent().getStringArrayListExtra("product_fats");
+        productSaturatedFatsArrayList = getIntent().getStringArrayListExtra("product_saturatedFats");
+        productProteinArrayList = getIntent().getStringArrayListExtra("product_protein");
+    }
+    private void sumAndViewMacros(){
         for (int i = 0; i < productCaloriesArrayList.size(); i++) {
             caloriesSum += parseDouble(productCaloriesArrayList.get(i));
             carbohydratesSum += parseDouble(productCarbohydratesArrayList.get(i));
@@ -71,29 +133,5 @@ public class ComposingDishesActivity extends AppCompatActivity {
         textViewComposhingDishesFats.setText(format.format(fatsSum) + "g\nfats");
         textViewComposhingDishesSaturatedFats.setText(format.format(saturatedFatsSum) + "g\nsaturated fats");
         textViewComposhingDishesProtein.setText(format.format(proteinSum) + "g\nprotein");
-    }
-
-    private void initRecyclerView() {
-        RecyclerView recyclerView = findViewById(R.id.composingDishesRecyclerView);
-        RecyclerViewAdapter2 adapter = new RecyclerViewAdapter2(this, mProductImage, productName, productCaloriesArrayList, productCarbohydratesArrayList,
-                productSugarArrayList, productFatsArrayList, productSaturatedFatsArrayList, productProteinArrayList);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        if(productName.isEmpty()){
-            textViewNoData.setVisibility(View.VISIBLE);
-        }
-    }
-
-    private void getIncomingIntent() {
-        productName = getIntent().getStringArrayListExtra("product_name");
-        imageUrl = getIntent().getStringExtra("product_image");
-        productCaloriesArrayList = getIntent().getStringArrayListExtra("product_calories");
-        productGramArrayList = getIntent().getStringArrayListExtra("product_gram");
-        productCarbohydratesArrayList = getIntent().getStringArrayListExtra("product_carbohydrates");
-        productSugarArrayList = getIntent().getStringArrayListExtra("product_sugar");
-        productFatsArrayList = getIntent().getStringArrayListExtra("product_fats");
-        productSaturatedFatsArrayList = getIntent().getStringArrayListExtra("product_saturatedFats");
-        productProteinArrayList = getIntent().getStringArrayListExtra("product_protein");
     }
 }
