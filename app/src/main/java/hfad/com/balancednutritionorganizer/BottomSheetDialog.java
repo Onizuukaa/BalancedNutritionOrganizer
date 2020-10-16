95a911fad1f6a2b9705ec987c9db7d14309ddd73
Dawid Hajder <dawidhajder@gmail.com>
2020-10-16 17:16:04 +0200

    Update before add search function for ComposedMealsActivity (2)

--- app/src/main/java/hfad/com/balancednutritionorganizer/BottomSheetDialog.java
+++ app/src/main/java/hfad/com/balancednutritionorganizer/BottomSheetDialog.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
