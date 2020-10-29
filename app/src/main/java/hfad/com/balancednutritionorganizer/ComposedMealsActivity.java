e27b08705da09ca40669de1b35bf641256807302
Dawid Hajder <dawidhajder@gmail.com>
2020-10-30 00:20:16 +0100

    After insert data to db ComposedDailyMeals

--- app/src/main/java/hfad/com/balancednutritionorganizer/ComposedMealsActivity.java
+++ app/src/main/java/hfad/com/balancednutritionorganizer/ComposedMealsActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -21,6 +22,8 @@ import android.widget.Toast;
 import java.util.List;
 
 import hfad.com.balancednutritionorganizer.adapters.RecyclerViewComposedMealsAdapter;
+import hfad.com.balancednutritionorganizer.database_things.ComposedDailyMealsColumns;
+import hfad.com.balancednutritionorganizer.database_things.ComposedDailyMealsDBHelper;
 import hfad.com.balancednutritionorganizer.database_things.ComposedMealsColumns;
 import hfad.com.balancednutritionorganizer.database_things.ComposedMealsDBHelper;
 
@@ -29,13 +32,15 @@ import static java.lang.Integer.parseInt;
 
 public class ComposedMealsActivity extends AppCompatActivity implements BottomSheetDialog.BottomSheetListener {
     RecyclerView recyclerView;
-    private SQLiteDatabase mDatabaseComposedMeals;
+    private SQLiteDatabase mDatabaseComposedMeals, mDatabaseComposedDailyMeals;
     private RecyclerViewComposedMealsAdapter adapter;
     Cursor cursor;
-    String a;
+    double kcal_Sum = 0.0, weight_Sum = 0.0, carbohydrates_Sum = 0.0, sugar_Sum = 0.0,
+            fats_Sum = 0.0, saturatedFats_Sum = 0.0, protein_Sum = 0.0;
     Button button_addMealsToDailyMenu;
-    EditText editText_removeMeal;
+    EditText editText_removeMeal, editText_dailyMealName;
     TextView textViewNoDataComposed;
+    String dailyMealName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +50,11 @@ public class ComposedMealsActivity extends AppCompatActivity implements BottomSh
         setTitle(R.string.Composed_Meals);
 
         ComposedMealsDBHelper dbHelperComposedMeals = new ComposedMealsDBHelper(this);
+        ComposedDailyMealsDBHelper dbHelperComposedDailyMeals = new ComposedDailyMealsDBHelper(this);
         mDatabaseComposedMeals = dbHelperComposedMeals.getWritableDatabase();
+        mDatabaseComposedDailyMeals = dbHelperComposedDailyMeals.getWritableDatabase();
         initRecyclerView();
-
+        editText_dailyMealName = findViewById(R.id.editText_dailyMealName);
         editText_removeMeal = findViewById(R.id.editText_removeItem_ComposedMeals);
         textViewNoDataComposed = findViewById(R.id.textViewNoDataComposed);
 
@@ -58,34 +65,109 @@ public class ComposedMealsActivity extends AppCompatActivity implements BottomSh
             @Override
             public void onClick(View v) {
                 List<ReturnItemComposedMeals> selectedMeals = adapter.getSelectedMeals();
+                ContentValues cv = new ContentValues();
+                dailyMealName = editText_dailyMealName.getText().toString();
 
-
-
-
-
-
-                StringBuilder composedMealsNames = new StringBuilder();
-                StringBuilder composedMealsCalories = new StringBuilder();
-                double sum = 0.0;
+//                StringBuilder composedMealsNames = new StringBuilder();
+//                StringBuilder composedMealsCalories = new StringBuilder();
                 for (int i = 0; i < selectedMeals.size(); i++) {
-                    if (i == 0) {
-                        //sum+= parseInt(selectedMeals.get(i).getProductCalories());
-                        //composedMealsNames.append(selectedMeals.get(i).getProductName());
-                        composedMealsCalories.append(selectedMeals.get(i).getProductCalories());
+                    // if (i == 0) {
 
-                    a = String.valueOf(selectedMeals.get(i).getProductCalories());
-                    sum += parseDouble(a);
-                       //sum += parseInt(composedMealsCalories.append(selectedMeals.get(i).getProductCalories()))   ;
-                    } else {
-                        //composedMealsNames.append("\n").append(selectedMeals.get(i).getProductName());
-                        composedMealsCalories.append("\n").append(selectedMeals.get(i).getProductCalories());
-                        a = String.valueOf(selectedMeals.get(i).getProductCalories());
-                        sum+=parseDouble(a);
-                    }
+                    //composedMealsNames.append(selectedMeals.get(i).getProductName());
+                    //composedMealsCalories.append(selectedMeals.get(i).getProductWeight());
+                    kcal_Sum += parseDouble(selectedMeals.get(i).getProductCalories());
+                    weight_Sum += parseDouble(selectedMeals.get(i).getProductWeight());
+                    carbohydrates_Sum += parseDouble(selectedMeals.get(i).getProductCarbohydrates());
+                    sugar_Sum += parseDouble(selectedMeals.get(i).getProductSugar());
+                    fats_Sum += parseDouble(selectedMeals.get(i).getProductFats());
+                    saturatedFats_Sum += parseDouble(selectedMeals.get(i).getProductSaturatedFats());
+                    protein_Sum += parseDouble(selectedMeals.get(i).getProductProtein());
+
+                    // } else {
+                    //composedMealsNames.append("\n").append(selectedMeals.get(i).getProductName());
+                    //composedMealsCalories.append("\n").append(selectedMeals.get(i).getProductWeight());
+//                        kcal_Sum += parseDouble(selectedMeals.get(i).getProductWeight());
+//                        weight_Sum += parseDouble(selectedMeals.get(i).getProductWeight());
+                    // }
                 }
-               //Toast.makeText(ComposedMealsActivity.this, composedMealsNames.toString(), Toast.LENGTH_SHORT).show();
-                Toast.makeText(ComposedMealsActivity.this, composedMealsCalories.toString(), Toast.LENGTH_SHORT).show();
-                Toast.makeText(ComposedMealsActivity.this, sum+"", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(ComposedMealsActivity.this, composedMealsNames.toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(ComposedMealsActivity.this, composedMealsCalories.toString(), Toast.LENGTH_SHORT).show();
+                //Toast.makeText(ComposedMealsActivity.this, kcal_Sum + "", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(ComposedMealsActivity.this, weight_Sum + "", Toast.LENGTH_SHORT).show();
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_composedDailyMeals_Name, dailyMealName);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_KCAL_SUM, kcal_Sum);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_WEIGHT_SUM, weight_Sum);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_CARBOHYDRATES_SUM, carbohydrates_Sum);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_SUGAR_SUM, sugar_Sum);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_FATS_SUM, fats_Sum);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_saturatedFATS_SUM, saturatedFats_Sum);
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ComposedDailyMeals_PROTEIN_SUM, protein_Sum);
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_NAME, selectedMeals.get(0).getProductName());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_NAME, selectedMeals.get(1).getProductName());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_NAME, selectedMeals.get(2).getProductName());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_NAME, selectedMeals.get(3).getProductName());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_NAME, selectedMeals.get(4).getProductName());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_KCAL, selectedMeals.get(0).getProductCalories());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_KCAL, selectedMeals.get(1).getProductCalories());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_KCAL, selectedMeals.get(2).getProductCalories());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_KCAL, selectedMeals.get(3).getProductCalories());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_KCAL, selectedMeals.get(4).getProductCalories());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_WEIGHT, selectedMeals.get(0).getProductWeight());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_WEIGHT, selectedMeals.get(1).getProductWeight());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_WEIGHT, selectedMeals.get(2).getProductWeight());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_WEIGHT, selectedMeals.get(3).getProductWeight());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_WEIGHT, selectedMeals.get(4).getProductWeight());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_CARBOHYDRATES, selectedMeals.get(0).getProductCarbohydrates());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_CARBOHYDRATES, selectedMeals.get(1).getProductCarbohydrates());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_CARBOHYDRATES, selectedMeals.get(2).getProductCarbohydrates());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_CARBOHYDRATES, selectedMeals.get(3).getProductCarbohydrates());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_CARBOHYDRATES, selectedMeals.get(4).getProductCarbohydrates());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_SUGAR, selectedMeals.get(0).getProductSugar());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_SUGAR, selectedMeals.get(1).getProductSugar());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_SUGAR, selectedMeals.get(2).getProductSugar());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_SUGAR, selectedMeals.get(3).getProductSugar());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_SUGAR, selectedMeals.get(4).getProductSugar());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_FATS, selectedMeals.get(0).getProductFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_FATS, selectedMeals.get(1).getProductFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_FATS, selectedMeals.get(2).getProductFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_FATS, selectedMeals.get(3).getProductFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_FATS, selectedMeals.get(4).getProductFats());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_saturatedFATS, selectedMeals.get(0).getProductSaturatedFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_saturatedFATS, selectedMeals.get(1).getProductSaturatedFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_saturatedFATS, selectedMeals.get(2).getProductSaturatedFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_saturatedFATS, selectedMeals.get(3).getProductSaturatedFats());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_saturatedFATS, selectedMeals.get(4).getProductSaturatedFats());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FirstProduct_PROTEIN, selectedMeals.get(0).getProductProtein());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_SecondProduct_PROTEIN, selectedMeals.get(1).getProductProtein());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_ThirdProduct_PROTEIN, selectedMeals.get(2).getProductProtein());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FourthProduct_PROTEIN, selectedMeals.get(3).getProductProtein());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_FifthProduct_PROTEIN, selectedMeals.get(4).getProductProtein());
+
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_productsIncludedFirstProduct, selectedMeals.get(0).getProductMacros());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_productsIncludedSecondProduct, selectedMeals.get(1).getProductMacros());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_productsIncludedThirdProduct, selectedMeals.get(2).getProductMacros());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_productsIncludedFourthProduct, selectedMeals.get(3).getProductMacros());
+                cv.put(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.COLUMN_productsIncludedFifthProduct, selectedMeals.get(4).getProductMacros());
+                mDatabaseComposedDailyMeals.insert(ComposedDailyMealsColumns.ComposedDailyMealsColumnsEntry.TABLE_NAME, null, cv);
+
+                Toast.makeText(ComposedMealsActivity.this, "Addded to database", Toast.LENGTH_SHORT).show();
+
+                kcal_Sum = 0.0;
+                weight_Sum = 0.0;
+                carbohydrates_Sum = 0.0;
+                sugar_Sum = 0.0;
+                fats_Sum = 0.0;
+                saturatedFats_Sum = 0.0;
+                protein_Sum = 0.0;
             }
         });
     }
