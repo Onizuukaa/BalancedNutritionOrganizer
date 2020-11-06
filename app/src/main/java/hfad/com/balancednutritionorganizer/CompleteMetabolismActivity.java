5aae30813feacb4405a82e9892b7397b0eec83cd
Dawid Hajder <dawidhajder@gmail.com>
2020-11-06 01:04:29 +0100

    Before modify add water

--- app/src/main/java/hfad/com/balancednutritionorganizer/CompleteMetabolismActivity.java
+++ app/src/main/java/hfad/com/balancednutritionorganizer/CompleteMetabolismActivity.java
@@ -34,6 +34,7 @@ public class CompleteMetabolismActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_complete_metabolism);
+        setTitle(R.string.Metabolism_Calculator);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         initViews();
